058652e1ad46a842055a7a6a7e02be10ce2a2081
LuboGolemiq <lubomirpalazov19@gmail.com>
2024-02-25 23:31:27 +0200

    Listovka

---
+++ List1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace List1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Dai chisla: ");
+            List<int> nums = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            int number = nums[nums.Count - 1];
+            while (nums.Contains(number))
+            {
+                nums.Remove(number);
+            }
+            Console.WriteLine(string.Join(", ", nums));
+        }
+    }
+    
+}
